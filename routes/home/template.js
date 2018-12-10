f000ff6dd84715634bd0501e55d89a1031b3d267
Daniel Belisle <daniel.belisle@aofl.com>
2018-12-10 15:06:41 -0800

    Create new home page
    Fix styles
    
    fixed lit-element version

--- routes/home/template.js
+++ routes/home/template.js
@@ -1,4 +1,32 @@
 export const template = (context, html) => html`
-  <h1>Welcome to aofl-js</h1>
-  <p>It works :)</p>
+  <a class="logo" href="https://ageoflearning.github.io/aofl" target="_blank" rel="noopener noreferrer">
+    <img width="200" src="https://ageoflearning.github.io/aofl/_assets/manifest/icon-512x512.png" alt="AofL JS Logo">
+  </a>
+
+  <div>
+    <a href="https://github.com/AgeOfLearning/aofl" target="_blank" rel="noopener noreferrer">
+      <img src="https://img.shields.io/github/tag/AgeOfLearning/aofl.svg">
+    </a>
+
+    <a href="https://travis-ci.org/AgeOfLearning/aofl" target="_blank" rel="noopener noreferrer">
+      <img src="https://travis-ci.org/AgeOfLearning/aofl.svg?branch=master">
+    </a>
+
+    <a href="https://codecov.io/gh/AgeOfLearning/aofl" target="_blank" rel="noopener noreferrer">
+      <img src="https://codecov.io/gh/AgeOfLearning/aofl/branch/master/graph/badge.svg" alt="Coverage Status" />
+    </a>
+
+    <a href="https://github.com/AgeOfLearning/aofl/blob/master/LICENSE.md" target="_blank" rel="noopener noreferrer">
+      <img src="https://img.shields.io/github/license/AgeOfLearning/aofl.svg">
+    </a>
+  </div>
+
+  <h1>Welcome to AofL JS</h1>
+
+  <p>This mono-repo contains tools and libraries for the AofL JS framework. For documentation visit the <a href="https://ageoflearning.github.io/aofl/" target="_blank" rel="noopener noreferrer no-wrap">AofL JS documentation website</a>.
+  </p>
+
+  <h2>Supported Browsers</h2>
+
+  <img src="https://saucelabs.com/browser-matrix/aoflsaucem.svg?auth=c0f586d708f2895cc0c009e385483adb" alt="Sauce Test Status"/>
 `;
